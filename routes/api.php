0cb77da5f1a4de95effe95158a26f201db7f79c9
mindara09 <ihsankarunia09@gmail.com>
2023-05-21 14:57:19 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v2')->group(function() {
 	Route::get('existing_personel/{id}', [App\Http\Controllers\LoginController::class, 'api_profile'])->name('api.profile');
 });
+
+Route::prefix('v1')->group(function() {
+	Route::get('/', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/personal_info', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/cadets', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/cadets/east_point_2011', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/candidate', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/candidate/2020', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/candidate/2021', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+	Route::get('/candidate/2022', function() {
+		return response()->json(['message' => 'success'],200);
+	});
+});
